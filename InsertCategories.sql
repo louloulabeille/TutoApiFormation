d1efd666e9a5324b1190b1a2c6c0d26904864241
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-20 14:36:00 +0100

    fichier requete sql pour ajouter les categories dans la base de données

---
+++ InsertCategories.sql
@@ -0,0 +1,32 @@
+INSERT INTO public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Développement', 'Apprenez le développement informatique.', 'icon_dev_100');
+
+INSERT INTO public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Design', 'Créer vos maquettes et logos.', 'icon_design_100');
+
+INSERT INTO public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Business', 'Lancez votre business.', 'icon_objectif_100');
+
+Insert Into public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Gestion', 'La gestion de projets.', 'icon_la_gestion_100');
+
+Insert Into public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Bureautique', 'Word. Powerpoint, Excel', 'icon_ms_powerpoint_100');
+
+	Insert Into public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Photo', 'La retouche photo.', 'icon_photo_100');
+
+
+	Insert Into public."Categorie"(
+	 "Title", "Message", "Image")
+	VALUES ('Vidéo', 'Le montage vidéo.', 'icon_video_100');
+
+	select * from public."Categorie"
+
+	
