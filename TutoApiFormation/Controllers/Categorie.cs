0d2b28362a5a9d9c17fb3455e8f834d4a1a2aa4e
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-20 13:39:23 +0100

    Mise en place de la migrations et mise en place du controleur

---
+++ TutoApiFormation/Controllers/Categorie.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TutoApiformation.Infrastructure.Database;
+
+namespace TutoApiFormation.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class Categorie(TutoApiDbContext context) : ControllerBase
+    {
+        #region private properties
+        private readonly TutoApiDbContext _context = context;
+        #endregion
+
+        #region methode controller
+        /// <summary>
+        /// Retourne toutes les categories de la base de données pour les 
+        /// injectées dans l'application
+        /// </summary>
+        /// <returns></returns>
+        [Route("GetAllCategories")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+
+            return this.BadRequest();
+        }
+
+        #endregion
+    }
+}
---
+++ TutoApiformation.Infrastructure/Migrations/20260320115128_InitDataBase.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TutoApiformation.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorie",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: true),
+                    Image = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorie", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorie");
+        }
+    }
+}
