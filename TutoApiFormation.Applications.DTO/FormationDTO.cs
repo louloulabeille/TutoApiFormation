c7c69cb5bd63be9d7b0cb7b3343872e768a82785
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-26 09:29:53 +0100

    Création du domain et du DTO de la formation

---
+++ TutoApiFormation.Applications.DTO/FormationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoApiFormation.Applications.DTO
+{
+    public class FormationDTO
+    {
+        public string? Name { get; set; }
+        public double? Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Tag { get; set; }
+    }
+}
---
+++ TutoApiFormation.Domain/Formation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TutoApiFormation.Domain
+{
+    public class Formation
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public double? Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Tag { get; set; }
+    }
+}
