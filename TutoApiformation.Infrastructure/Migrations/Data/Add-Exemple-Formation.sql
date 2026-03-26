3023431641e22f39d1faacaa2f92981c4e7728dc
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-26 10:49:53 +0100

    ajout d'exemple de formations directement en sql dans la base de données

---
+++ TutoApiformation.Infrastructure/Migrations/Data/Add-Exemple-Formation.sql
@@ -0,0 +1,26 @@
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('C#', 15.90, 'Développement C#14 & .Net10', '1.png', 'Code', 1);
+
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('Unity', 29.90, 'Création de jeux ave Unity.', '2.png', 'Unity', 1);
+
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('Symphony', 24.90, 'Création d''un site marchant avec Synphony.', '3.png', 'Web', 1);
+
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('C++', 49.90, 'Langage Machine avancé, Apprennez le c++.', '4.png', 'Code', 1);
+
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('Figma', 19.90, 'Design avec Figma de A - Z avec des exemples concrets.', '5.png', 'Design', 1);
+
+INSERT INTO public."Formation"(
+	"Name", "Price", "Description", "ImageUrl", "Tag", "CategorieId")
+	VALUES ('MAUI', 15.90, 'Développement sur Android & Ios en C# et création d''une API.', '6.png', 'Code', 1);
+
+	select * from public."Formation";
+	
