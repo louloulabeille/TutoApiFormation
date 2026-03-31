7160ae51445704555fd4bb6d52ec7aa0fc750ab3
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-03-31 12:28:20 +0200

    Mise en place de la popup de connexion.

---
+++ TutoApiFormation.Applications.DTO/Http/ResultHttp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TutoApiFormation.Applications.DTO.Infrastructure;
+
+namespace TutoApiFormation.Applications.DTO.Http 
+{ 
+    public class ResultHttp
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string? HttpContentMessage { get; set; }
+        public IdentityDTO? IdentityDTO { get; set; }
+    }
+}
